89c3faa5d51b3e39d9ace301e9b42aa08032f462
MarcoV <marco.verzeni@mail.polimi.it>
2022-07-24 18:55:35 +0200

    fix useContext type

--- context.tsx
+++ context.tsx
@@ -1,7 +1,9 @@
 import { createContext, useState } from "react";
 import { storedProductData } from "./components/ProductForm";
 
-export const AppContext = createContext({});
+export const AppContext = createContext<
+  [storedProductData[], React.Dispatch<React.SetStateAction<storedProductData[]>>]
+>([[], (x) => x]);
 
 export const AppProvider: React.FC = (props) => {
   const [items, setItems] = useState<storedProductData[]>([]);
